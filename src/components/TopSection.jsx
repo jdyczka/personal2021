fb8333fb704401caf1677e6388f022b20bba6d27
jdyczka <asia.dyczka@gmail.com>
2021-08-01 21:12:56 +0200

    add top-section layout

--- src/components/TopSection.jsx
+++ src/components/TopSection.jsx
@@ -1,7 +1,11 @@
 export default function TopSection() {
 
     return (<section className="top-section">
-        <h1>Hi, I'm Joanna</h1>
-
+        <p>Hi, my name is</p>
+        <h1>Joanna Dyczka</h1>
+        <div className="frame">
+            <p className="subtitle">I am a web designer & developer based in Lodz, Poland</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ea officiis tempora iure qui aliquid soluta natus, deleniti velit sed! Laudantium nisi fuga corrupti dolore est animi nobis nulla voluptatem.</p>
+        </div>
     </section>);
 }
